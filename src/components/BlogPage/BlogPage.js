35653d25f06be5dd7052ddfa587b407d55470abf
francaboy <srdjan.msevic@gmail.com>
2024-06-24 14:57:53 +0200

    update
    cards updated

--- src/components/BlogPage/BlogPage.js
+++ src/components/BlogPage/BlogPage.js
@@ -2,149 +2,150 @@ import SingleBlog from "@/components/BlogOne/SingleBlog";
 import blogPage from "@/data/blogPage";
 import Link from "next/link";
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { Image } from "react-bootstrap";
 
 const BlogPage = () => {
   return (
     <section className="blog-page">
       <Container>
-        <Row>
-        <Col xl={4} lg={4} className="animated fadeInUp">
-          <div className="blog-one__single">
-          <div className="blog-one__img">
-          <Image src={require(`@/images/blog/blog-page-img-1.jpg`).default.src} alt="" />
-          <Link href="/nikola-rokvic" legacyBehavior>
-            <a>
-              <span className="blog-one__plus"></span>
-            </a>
-          </Link>
-          <div className="blog-one__date">
-            <p>8. Август</p>
-          </div>
-        </div>
-        <div className="blog-content">
-          <ul className="list-unstyled blog-one__meta">
-            <li>
+        <div className="blog-one__container">
+          <div className="blog-one__single animated fadeInUp">
+            <div className="blog-one__img">
+              <Image src={require(`@/images/blog/blog-page-img-1.jpg`).default.src} alt="" />
               <Link href="/nikola-rokvic" legacyBehavior>
                 <a>
-                  <i className="far fa-user-circle"></i> админ
+                  <span className="blog-one__plus"></span>
                 </a>
               </Link>
-            </li>
-            <li>
-              <span>/</span>
-            </li>
-            <li>
-              <Link href="/nikola-rokvic" legacyBehavior>
-                <a>
-                  <i className="far fa-comments"></i> 0 Коментара
-                </a>
-              </Link>
-            </li>
-          </ul>
-          <h3 className="blog-one__title">
-            <Link href="/nikola-rokvic" legacyBehavior>
-              Никола Роквић
-            </Link>
-          </h3>
-          <div className="blog-one__read-btn">
-            <Link href="/nikola-rokvic" legacyBehavior>
-              Више детаља
-            </Link>
+              <div className="blog-one__date">
+                <p>8. Август</p>
+              </div>
+            </div>
+            <div className="blog-content">
+              <ul className="list-unstyled blog-one__meta">
+                <li>
+                  <Link href="/nikola-rokvic" legacyBehavior>
+                    <a>
+                      <i className="far fa-user-circle"></i> админ
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <span>/</span>
+                </li>
+                <li>
+                  <Link href="/nikola-rokvic" legacyBehavior>
+                    <a>
+                      <i className="far fa-comments"></i> 0 Коментара
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+              <h3 className="blog-one__title">
+                <Link href="/nikola-rokvic" legacyBehavior>
+                  Никола Роквић
+                </Link>
+              </h3>
+              <div className="blog-one__read-btn">
+                <Link href="/nikola-rokvic" legacyBehavior>
+                  Више детаља
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="blog-one__single">
-        <div className="blog-one__img">
-          <Image src={require(`@/images/blog/blog-page-img-2.jpg`).default.src} alt="" />
-          <Link href="/zorica-brunclik" legacyBehavior>
-            <a>
-              <span className="blog-one__plus"></span>
-            </a>
-          </Link>
-          <div className="blog-one__date">
-            <p>9. Август</p>
-          </div>
-        </div>
-        <div className="blog-content">
-          <ul className="list-unstyled blog-one__meta">
-            <li>
+
+          <div className="blog-one__single animated fadeInUp">
+            <div className="blog-one__img">
+              <Image src={require(`@/images/blog/blog-page-img-2.jpg`).default.src} alt="" />
               <Link href="/zorica-brunclik" legacyBehavior>
                 <a>
-                  <i className="far fa-user-circle"></i> админ
+                  <span className="blog-one__plus"></span>
                 </a>
               </Link>
-            </li>
-            <li>
-              <span>/</span>
-            </li>
-            <li>
-              <Link href="/zorica-brunclik" legacyBehavior>
-                <a>
-                  <i className="far fa-comments"></i> 0 Коментара
-                </a>
-              </Link>
-            </li>
-          </ul>
-          <h3 className="blog-one__title">
-            <Link href="/zorica-brunclik" legacyBehavior>
-              Зорица Брунцлик
-            </Link>
-          </h3>
-          <div className="blog-one__read-btn">
-            <Link href="/zorica-brunclik" legacyBehavior>
-              Више детаља
-            </Link>
+              <div className="blog-one__date">
+                <p>9. Август</p>
+              </div>
+            </div>
+            <div className="blog-content">
+              <ul className="list-unstyled blog-one__meta">
+                <li>
+                  <Link href="/zorica-brunclik" legacyBehavior>
+                    <a>
+                      <i className="far fa-user-circle"></i> админ
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <span>/</span>
+                </li>
+                <li>
+                  <Link href="/zorica-brunclik" legacyBehavior>
+                    <a>
+                      <i className="far fa-comments"></i> 0 Коментара
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+              <h3 className="blog-one__title">
+                <Link href="/zorica-brunclik" legacyBehavior>
+                  Зорица Брунцлик
+                </Link>
+              </h3>
+              <div className="blog-one__read-btn">
+                <Link href="/zorica-brunclik" legacyBehavior>
+                  Више детаља
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="blog-one__single">
-        <div className="blog-one__img">
-          <Image src={require(`@/images/blog/blog-page-img-3.jpg`).default.src} alt="" />
-          <Link href="/baja-mali-knindza" legacyBehavior>
-            <a>
-              <span className="blog-one__plus"></span>
-            </a>
-          </Link>
-          <div className="blog-one__date">
-            <p>10. Август</p>
-          </div>
-        </div>
-        <div className="blog-content">
-          <ul className="list-unstyled blog-one__meta">
-            <li>
+
+          <div className="blog-one__single animated fadeInUp">
+            <div className="blog-one__img">
+              <Image src={require(`@/images/blog/blog-page-img-3.jpg`).default.src} alt="" />
               <Link href="/baja-mali-knindza" legacyBehavior>
                 <a>
-                  <i className="far fa-user-circle"></i> админ
+                  <span className="blog-one__plus"></span>
                 </a>
               </Link>
-            </li>
-            <li>
-              <span>/</span>
-            </li>
-            <li>
-              <Link href="/baja-mali-knindza" legacyBehavior>
-                <a>
-                  <i className="far fa-comments"></i> 0 Коментара
-                </a>
-              </Link>
-            </li>
-          </ul>
-          <h3 className="blog-one__title">
-            <Link href="/baja-mali-knindza" legacyBehavior>
-              Баја Мали Книнџа
-            </Link>
-          </h3>
-          <div className="blog-one__read-btn">
-            <Link href="/baja-mali-knindza" legacyBehavior>
-              Више детаља
-            </Link>
+              <div className="blog-one__date">
+                <p>10. Август</p>
+              </div>
+            </div>
+            <div className="blog-content">
+              <ul className="list-unstyled blog-one__meta">
+                <li>
+                  <Link href="/baja-mali-knindza" legacyBehavior>
+                    <a>
+                      <i className="far fa-user-circle"></i> админ
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <span>/</span>
+                </li>
+                <li>
+                  <Link href="/baja-mali-knindza" legacyBehavior>
+                    <a>
+                      <i className="far fa-comments"></i> 0 Коментара
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+              <h3 className="blog-one__title">
+                <Link href="/baja-mali-knindza" legacyBehavior>
+                  Баја Мали Книнџа
+                </Link>
+              </h3>
+              <div className="blog-one__read-btn">
+                <Link href="/baja-mali-knindza" legacyBehavior>
+                  Више детаља
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </Col>
-        </Row>
+
         <div className="blog-sidebar__load-more text-center">
           <Link href="#" legacyBehavior>
             <a className="thm-btn blog-sidebar__load-more-btn">
